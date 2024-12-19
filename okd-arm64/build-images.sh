3b035ed7625a5a7d647fb01208b80e2833dd8181
dsedgmen <52942401+dsedgmen@users.noreply.github.com>
2024-12-19 16:27:40 +1000

    Update build-images.sh

--- okd-arm64/build-images.sh
+++ okd-arm64/build-images.sh
@@ -207,7 +207,8 @@ containernetworking_plugins_microshift_image() {
   # Apply the sed commands for the service-ca-operator Dockerfile
   sed -i 's|^FROM registry.ci.openshift.org/ocp/builder.*|FROM registry.ci.openshift.org/openshift/release:rhel-9-release-golang-1.21-openshift-4.16 AS builder|' "$dockerfile_path"
   sed -i "s|^FROM registry.ci.openshift.org/ocp/.*:base-rhel9|FROM quay.io/redhat_emp1/okd-arm/scos-${OKD_VERSION}:base-stream9|" "$dockerfile_path"
-
+  sed -i 's|dnf install -y |dnf --disablerepo=rt install -y |' "$dockerfile_path"
+  
   podman build --platform linux/arm64 -t "${images[containernetworking-plugins-microshift]}" -f "$dockerfile_path" .
   podman push "${images[containernetworking-plugins-microshift}"
 
@@ -226,7 +227,8 @@ multus_cni_microshift_image() {
   # Apply the sed commands for the service-ca-operator Dockerfile
   sed -i 's|^FROM registry.ci.openshift.org/ocp/builder.*|FROM registry.ci.openshift.org/openshift/release:rhel-9-release-golang-1.21-openshift-4.16 AS builder|' "$dockerfile_path"
   sed -i "s|^FROM registry.ci.openshift.org/ocp/.*:base-rhel9|FROM quay.io/redhat_emp1/okd-arm/scos-${OKD_VERSION}:base-stream9|" "$dockerfile_path"
-
+  sed -i 's|dnf install -y |dnf --disablerepo=rt install -y |' "$dockerfile_path"
+  
   podman build --platform linux/arm64 -t "${images[multus-cni-microshift]}" -f "$dockerfile_path" .
   podman push "${images[multus-cni-microshift}"
 
